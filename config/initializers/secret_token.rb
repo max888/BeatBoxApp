420dc0182b427485d9523bdda4625afc2260b0ec
Max Norton <Max@Maxs-MacBook-Pro.local>
2014-02-03 09:26:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeatboxApp::Application.config.secret_token = '39a7910109e90185b9da1a799821850d6322c49a6f4c5d5c2763f6ebf8dedae282f260e9fccdb7140804fb878811e548967b87accfd78c46dade4fbb0ca7f64a'
